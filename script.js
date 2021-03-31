99079e46ad2fc500f288674ebd4233b02c2b5e3f
Asad Siddiquee <asadsidn@gmail.com>
2021-03-31 19:05:29 +0200

    initiating upload

---
+++ script.js
@@ -0,0 +1,60 @@
+ 
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'style.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
+
+// Auto text
+
+const textEl = document.getElementById("my-info")
+
+let idx = 1
+
+const text = 'Ex-(Electrical and Electronic Engineer) & A Wannabe FrontEnd Web Developer! I LOVE PROGRAMMING!!!!!!!'
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice(0,idx)
+    idx++
+
+    if(idx>text.length){
+        idx=1
+    }
+    setTimeout(writeText,100)
+}
